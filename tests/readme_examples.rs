6214456ddd50d8703a533fc80c89cf97c68d32d2
Kirill Kuzminykh <cykooz@gmail.com>
2021-07-04 16:45:44 +0300

    Added some comments for example code

--- tests/readme_examples.rs
+++ tests/readme_examples.rs
@@ -5,21 +5,28 @@ use fast_image_resize::{
 };
 
 fn resize_lanczos3(src_pixels: &[u8], width: NonZeroU32, height: NonZeroU32) -> Vec<u8> {
+    // Create wrapper for raw data of source image
     let src_image = ImageData::new(width, height, src_pixels, PixelType::U8x4).unwrap();
+    // Get immutable view of image data
     let src_view = src_image.src_view();
 
     let dst_width = NonZeroU32::new(1024).unwrap();
     let dst_height = NonZeroU32::new(768).unwrap();
+    // Create wrapper that own data of destination image
     let mut dst_image = ImageData::new_owned(dst_width, dst_height, src_image.pixel_type());
+    // Get mutable view of destination image data
     let mut dst_view = dst_image.dst_view();
 
+    // Create Resizer instance and resize source image into buffer of destination image
     let mut resizer = Resizer::new(ResizeAlg::Convolution(FilterType::Lanczos3));
     resizer.resize(&src_view, &mut dst_view);
 
+    // Return destination buffer as Vec<u8>
     dst_image.get_buffer().to_owned()
 }
 
 fn crop_and_resize_image(mut src_view: SrcImageView) -> ImageData<Vec<u8>> {
+    // Set crop-box for view of source image
     src_view
         .set_crop_box(CropBox {
             left: 10,
